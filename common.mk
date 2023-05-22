1240a151748edf1e1dec4ac0c5e54949b412a5bc
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-23 01:38:35 +0800

    sm8350-common: Move powerhint copy operation to device-specific tree
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -354,9 +354,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
-
 $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-xiaomi)
 
 # PowerShare
