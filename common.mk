2b6e7f01f1abb601d829d801d99b8bb229bbb750
EndCredits <endcredits@crepuscular-aosp.icu>
2023-05-22 23:39:00 +0800

    sm8350-common: Mirror shima powerhint from lahaina
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- common.mk
+++ common.mk
@@ -355,7 +355,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-xiaomi)
 
